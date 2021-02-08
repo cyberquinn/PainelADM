b22d9385502bc9ec63b64f9c88d30191d3f571a1
PROG WEB - MANHA <PROG WEB - MANHA@MININT-633QJ9F>
2021-02-08 12:52:57 -0200

    Aula dia 08/02

---
+++ site/includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Meu site</title>
+    
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="site/assets/bootstrap/css/bootstrap.min.css" />
+    <!--Plugins-->
+    <link rel="stylesheet" href="../../site/" />
+    <!--Mus css-->
+</head>
+
+<body>
+    
+    <!--jQuery-->
+    <script src="site/assets/js/jquery.js"></script>    
+    <!--Bootstrap-->
+    
+    <!--Funções JaveScript-->
+    
+</body>
+</html>
